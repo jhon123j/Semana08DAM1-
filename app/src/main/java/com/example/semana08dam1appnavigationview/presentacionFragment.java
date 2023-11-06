4c2e92f5590fa6b78744a16dc9f60e591f1c5d3a
jhon123j <jhonfloresr@gmail.com>
2023-11-06 17:18:00 -0500

    semana08

---
+++ app/src/main/java/com/example/semana08dam1appnavigationview/presentacionFragment.java
@@ -0,0 +1,19 @@
+package com.example.semana08dam1appnavigationview;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class presentacionFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_presentacion, container, false);
+    }
+}
